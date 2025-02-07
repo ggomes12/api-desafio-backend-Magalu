7e8b73cf3703cb5e900e9e1fe93f14ace7165344
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-02-07 20:43:41 -0300

    init: first commit

---
+++ src/main/java/com/ggomes/notificacao_agendada_api/NotificacaoAgendadaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ggomes.notificacao_agendada_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoAgendadaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoAgendadaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggomes/notificacao_agendada_api/NotificacaoAgendadaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomes.notificacao_agendada_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoAgendadaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
